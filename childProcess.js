11de1ef0f014a94409b163369c4c8c9c6bf7ce8a
zuhaBAW <zuhabaw@gmail.com>
2020-09-08 14:21:44 +0530

    test

---
+++ childProcess.js
@@ -0,0 +1,66 @@
+const cp = require('child_process');
+
+const exec_options = {
+    cwd: null,
+    env: null,
+    encoding: 'utf8',
+    timeout: 0,
+    maxBuffer: 200 * 1024,
+    killSignal: 'SIGTERM'
+};
+
+// exec
+cp.exec('ls ', exec_options, (err, stdout, stderr) => {
+    console.log('#1. exec')
+    console.log(stdout);
+});
+
+try {
+    const data = cp.execSync('ls', exec_options);
+    console.log('#2. exec sync')
+    console.log(data.toString());
+} catch (err) {
+
+}
+
+
+const spawn_options = {
+    cwd: null,
+    env: null,
+    detached: false
+};
+
+const spawn_options = {
+    cwd: null,
+    env: null,
+    detached: false
+};
+
+// spawn
+const ls = cp.spawn('ls', ['-l'], spawn_options);
+
+ls.stdout.on('data', stdout => {
+    console.log('#3. spawn')
+    console.log(stdout.toString());
+});
+
+ls.stderr.on('data', stderr => {
+    console.log(stderr.toString());
+});
+
+ls.on('close', code => {
+    // ended with code
+});
+
+
+// spawn sync
+const { stdout, stderr, pid, status } = cp.spawnSync('ls', ['-l'], spawn_options);
+console.log('#4. spawn sync')
+console.log(stdout.toString());
+
+api(){
+  sp = spawn(n,,adsf,asdf,asdf)
+  sp.on('stop', askdfhasdkfj)
+  .on(asdfasdfasdf)
+  .=sp.
+}
